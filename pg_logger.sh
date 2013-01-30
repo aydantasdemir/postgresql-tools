bfaeca969d0c3636a292b1122991f1c2af903045
Erkin <travijuu@gmail.com>
2013-01-30 16:36:23 +0200

    files added.

--- pg_logger.sh
+++ pg_logger.sh
@@ -2,14 +2,21 @@
 
 MACHINE=$(hostname)
 VERSION="9.2"
-LOGPATH="/var/log/pgsql/$VERSION/data/pg_log"
+LOGPATH="/var/lib/pgsql/$VERSION/data/pg_log"
+#LOGPATH="/var/lib/postgresql/$VERSION/main/pg_log"
 LOG="postgresql-$(date +%a -d 'yesterday').log"
-LOGDIR="/home/travego/pgLogger/pglogs/"
-OUTPUTDIR="/home/travego/pgLogger/finished/"
-OUTPUTLOG="$MACHINE-$LOG"
+LOGDIR="/home/travego/pgLogger/pglogs"
+OUTPUTDIR="/home/travego/pgLogger/finished"
+OUTPUTLOG="$MACHINE-$LOG.html"
 HOST="travego@10.22.2.110:"
+HOST="root@tarantino.vpn.akinon.net:"
+HOST="erkin.cakar@kirsten.vpn.akinon.net:"
 
+echo $LOGPATH/$LOG
+echo $LOGDIR/$LOG
+ 
+#mkdir -p $OUTPUTDIR
 
-rsync -av $LOGPATH/$LOG $LOGDIR/$COMINGLOG
+rsync -av $HOST$LOGPATH/$LOG $LOGDIR/$LOG
 pgfouine -file $LOGDIR/$LOG -logtype stderr > $OUTPUTDIR/$OUTPUTLOG
 
