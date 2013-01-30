80ecbc30c5771700976ee2426b20fdb074c5f824
Erkin <travijuu@gmail.com>
2013-01-30 16:42:02 +0200

    comments removed.

--- alter_index_tablespace.sh
+++ alter_index_tablespace.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#DB="warehouse"
-#TBLSPC="warehouse_idx_tblspc"
+DB="warehouse"
+TBLSPC="warehouse_idx_tblspc"
 
-#INDEXES=$(psql -U postgres $DB -At -c "select indexname from pg_indexes where schemaname !='pg_catalog';")
+INDEXES=$(psql -U postgres $DB -At -c "select indexname from pg_indexes where schemaname !='pg_catalog';")
 
-#for INDEX in $INDEXES
-#do
-#	echo "ALTER INDEX $INDEX SET TABLESPACE $TBLSPC;"
-#	psql -U postgres $DB -c "ALTER INDEX $INDEX SET TABLESPACE $TBLSPC;"
-#done
-
-
+for INDEX in $INDEXES
+do
+	echo "ALTER INDEX $INDEX SET TABLESPACE $TBLSPC;"
+	psql -U postgres $DB -c "ALTER INDEX $INDEX SET TABLESPACE $TBLSPC;"
+done
