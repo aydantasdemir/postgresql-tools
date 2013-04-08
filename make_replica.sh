0c80d4d697041e4a2689f5c2e84e6a5b0e1d939f
Erkin Çakar <erk.cakar@gmail.com>
2013-04-08 21:59:25 +0300

    comments added & error handling

--- make_replica.sh
+++ make_replica.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+#############################
+## Script: make_replica.sh ##
+## Coded by Erkin Çakar    ##
+## Date: 08-04-2013        ##
+#############################
+
+
 VERSION="9.2"
 HOST="10.22.22.138"
 USER="moveme"
 CONFPATH="~"
+DATADIR="/var/lib/pgsql/$VERSION/data"
+BASEBACKUP_BIN="/usr/pgsql-$VERSION/bin/pg_basebackup"
+OUTPUT="/dev/null"
 
-/etc/init.d/postgresql-$VERSION stop
+function throw_exception()
+{
+	if [ $? > 0 ]; then echo "ERROR: $1"; exit;	fi
+}
 
-rm -rf /var/lib/pgsql/$VERSION/data/*
+echo " ## Hot standby replication server is going to be settled down ##"
+/etc/init.d/postgresql-$VERSION stop 2> $OUTPUT || throw_exception "Cannot stop PostgreSQL!"
 
-/usr/pgsql-$VERSION/bin/pg_basebackup -h $HOST -x -U $USER -D /var/lib/pgsql/$VERSION/data/ -l new_replica
-
-cp $CONFPATH/postgresql.conf $CONFPATH/recovery.conf /var/lib/pgsql/$VERSION/data/
-
-chown -R postgres:postgres /var/lib/pgsql/$VERSION/data/*
+rm -rf $DATADIR/* || throw_exception "Cannot be deleted Data directory!"
+echo "   --> PostgreSQL data directory: \"$DATADIR\" is DELETED."
+echo "   --> pg_basebackup tool is starting..."
+BASEBACKUP_BIN -h $HOST -x -U $USER -D $DATADIR/ -l new_replica || throw_exception "Problem in pg_basebackup tool"
+echo "   --> pg_basebackup completed! √"
+cp $CONFPATH/postgresql.conf $CONFPATH/recovery.conf $DATADIR/ || throw_exception "Cannot be copied config files"
+echo "   --> postgresql.conf & recovery.conf is copied to data directory"
+chown -R postgres: $DATADIR/* || throw_exception "Owner of data directory cannot be changed!"
+echo "   --> The owner of data directory setted to postgres"
+echo " ## Hot standby PostgreSQL Server is ready to start! ##"
