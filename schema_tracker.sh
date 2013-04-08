8e70d79f57f4e3fcae5eb64857539fe95147b160
Erkin Çakar <erk.cakar@gmail.com>
2013-04-08 23:00:15 +0300

    tab reverted into space

--- schema_tracker.sh
+++ schema_tracker.sh
@@ -8,7 +8,7 @@ Example-1:  ./schema_tracker.sh --db dbname --user dbuser --email user@mail.com 
 Example-2:  ./schema_tracker.sh --db dbname --user dbuser --email user@mail.com -update
      
      Description: *** This helps to track database schema changes and to report the result to given email address ***
-     	   Notes: *** Action options should be the last parameter of script ***
+            Notes: *** Action options should be the last parameter of script ***
              
 OPTIONS:
   ACTION OPTIONS
@@ -34,70 +34,70 @@ email=""
 
 if [ $# -eq 0 ]
 then
-	usage
+    usage
 fi
 
 while [ $# -gt 0 ]
 do
-	exp=$(echo "$1" | grep '^-[[:lower:]]')
-	if [ "$1" = "$exp" ]
-	then
-		#echo $1
-		case "$1" in 
-		"-h")
-			usage
-			exit
-			;;
-		"-run")
-			pg_dump -s $db -U $dbuser -f $dump_file
-			;;
-		"-update")
-			if [ ! -e $dump_file ]
-			then
-				echo "ERROR: $db not exist. It will be created.."
-				pg_dump -s $db -U $dbuser -f $dump_file
-				exit
-			fi
+    exp=$(echo "$1" | grep '^-[[:lower:]]')
+    if [ "$1" = "$exp" ]
+    then
+        #echo $1
+        case "$1" in 
+        "-h")
+            usage
+            exit
+            ;;
+        "-run")
+            pg_dump -s $db -U $dbuser -f $dump_file
+            ;;
+        "-update")
+            if [ ! -e $dump_file ]
+            then
+                echo "ERROR: $db not exist. It will be created.."
+                pg_dump -s $db -U $dbuser -f $dump_file
+                exit
+            fi
 
-			pg_dump -s $db -U $dbuser -f $new_dump_file 
-			diff $dump_file $new_dump_file > $diff_file
-			if [  -s $diff_file ]
-			then
-				mail -s "$db schema changes" $email < $diff_file
-			fi
-			mv $new_dump_file $dump_file
-			rm $diff_file
-			;;
-		*)
-			echo "Unknown option: $1"
-			exit
-			;;
-		esac
-		shift
-	elif [ "$1" = $(echo "$1" | grep '^--[[:lower:]]') ]
-	then
-		case "$1" in
-		"--db")
-			db=$2
-			dump_file="$path/$db.dump"
-			new_dump_file="$path/$db.new.dump"
-			diff_file="$path/$db.diff"
-			;;
-		"--user")
-			dbuser=$2
-			;;
-		"--email")
-			email=$2
-			;;
-		*)
-			echo "Unknown option: $1"
-			exit
-			;;
-		esac
-		echo "  $1 is setted to $2"
-		shift 2
-	else
-		echo "Error $1: wrong parameter"	
-		exit
-	fi
+            pg_dump -s $db -U $dbuser -f $new_dump_file 
+            diff $dump_file $new_dump_file > $diff_file
+            if [  -s $diff_file ]
+            then
+                mail -s "$db schema changes" $email < $diff_file
+            fi
+            mv $new_dump_file $dump_file
+            rm $diff_file
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit
+            ;;
+        esac
+        shift
+    elif [ "$1" = $(echo "$1" | grep '^--[[:lower:]]') ]
+    then
+        case "$1" in
+        "--db")
+            db=$2
+            dump_file="$path/$db.dump"
+            new_dump_file="$path/$db.new.dump"
+            diff_file="$path/$db.diff"
+            ;;
+        "--user")
+            dbuser=$2
+            ;;
+        "--email")
+            email=$2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit
+            ;;
+        esac
+        echo "  $1 is setted to $2"
+        shift 2
+    else
+        echo "Error $1: wrong parameter"    
+        exit
+    fi
 done
