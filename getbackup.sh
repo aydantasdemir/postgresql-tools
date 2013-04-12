06fdb2eba8f0a61d6ea8d62eacb48435f8dce544
Erkin <travijuu@gmail.com>
2013-04-12 11:39:49 +0300

    file added.

--- getbackup.sh
+++ getbackup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DB="testdb"
+DB="travegodb"
 SERVER=$(hostname)
-PATH="/data/archives/backups"
-EMAIL="info@email.com"
+BCKP_PATH="/data/archives/backups"
+EMAIL="travego@localhost"
 
-find $PATH/ -name "*.dump" -exec rm {} \;
-pg_dump -U postgres -F c -b -f "$PATH/$(date +%a)-$DB.dump" $DB
+find /data/archives/backups/ -name "*.dump" -exec rm {} \;
+pg_dump -U postgres -F c -b -f "$BCKP_PATH/$(date +%a)-$DB.dump" $DB
 if [ $? > 0 ]
 then
-   ls -lh $PATH/$(date +%a)-$DB.dump | mail -s "PROBLEM: $DB($SERVER) dump" $EMAIL
+   ls -lh $BCKP_PATH/$(date +%a)-$DB.dump | mail -s "PROBLEM: $DB($SERVER) dump" $EMAIL
    exit;
 fi
-ls -lh $PATH/$(date +%a)-$DB.dump | mail -s "$DB($SERVER) dump complete" $EMAIL
+ls -lh $BCKP_PATH/$(date +%a)-$DB.dump | mail -s "$DB($SERVER) dump complete" $EMAIL
