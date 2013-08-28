5cc17e6f42d520907f8af0ab8e86d99dba13dfd7
Erkin <travijuu@gmail.com>
2013-08-28 09:30:59 +0300

    bug fix getting min max date range

--- partitionize.sh
+++ partitionize.sh
@@ -8,10 +8,10 @@
 
 
 ## USER DEFINED SETTING PARAMETERS ##
-DB="travegodb"
+DB="warehouse_daily_test"
 DBUSER="postgres"
-BASETABLE="part"
-DIVISION_COLUMN="created_at"
+BASETABLE="actstream_action"
+DIVISION_COLUMN="timestamp"
 INTERVAL="1 month"
 DEBUG=0 #DEBUG MODE
 ## USER DEFINED SETTING PARAMETERS ##
@@ -20,46 +20,18 @@ DEBUG=0 #DEBUG MODE
 TYPE=`echo $INTERVAL | cut -d" " -f2`
 TMP_BASETABLE="$BASETABLE"_tmp
 TRIGGER=$BASETABLE"_insert_trigger"
-START_DATE=`psql -U $DBUSER -d $DB -At -c "SELECT MIN($DIVISION_COLUMN) FROM $BASETABLE"`
-END_DATE=`psql -U $DBUSER -d $DB -At -c "SELECT MAX($DIVISION_COLUMN) FROM $BASETABLE"`
+START_DATE=`psql -U $DBUSER -d $DB -At -c "SELECT to_char(MIN($DIVISION_COLUMN),'YYYY-MM-01') FROM $BASETABLE"`
+END_DATE=`psql -U $DBUSER -d $DB -At -c "SELECT to_char( (SELECT MAX($DIVISION_COLUMN) + interval '1 month' FROM $BASETABLE),'YYYY-MM-01' )"`
+
 TRIGGER_FLAG=1
 
-throw_ex() { 
+throw_ex() {
 #if [ $? > 0 ]; then echo "ERROR: $1"; exit; fi 
-if [ $? > 0 ]; then if [ $DEBUG -eq 0 ]; then echo "ERROR: $1"; exit; fi fi 
+if [ $? > 0 ]; then if [ $DEBUG -eq 0 ]; then echo "ERROR: $1"; exit; fi fi
 }
 
-# Find the min and max date from the table
-DATE_RANGE=`psql -U $DBUSER -d $DB -At -c "SELECT to_char(generate_series('$START_DATE'::timestamp , '$END_DATE'::timestamp, '$INTERVAL'),'YYYY-MM-DD');"` ||throw_ex "Getting Date range array from DB"
-
-echo  '\nTABLE: '$BASETABLE' \nDATE BETWEEN: '$START_DATE' AND '$END_DATE' \nINTERVAL: '$INTERVAL' \nTOTAL TABLES: '${#DATE_RANGE}'\n'
- 
-TRIGGER_STATEMENT_START="
-CREATE OR REPLACE FUNCTION $TRIGGER()
-RETURNS TRIGGER AS \$\$
-BEGIN"
-
-TRIGGER_STATEMENT_END="
-    END IF;
-RETURN NULL;
-END;
-\$\$
-LANGUAGE plpgsql;COMMIT;
-"
-
-TRIGGER_STATEMENT_BODY=""
- 
-# RENAME BASE TABLE AS TMP TABLE
-psql -U $DBUSER $DB -c "ALTER TABLE $BASETABLE RENAME TO $TMP_BASETABLE;" > /dev/null ||throw_ex "Cannot renamed the table $BASETABLE to $TMP_BASETABLE" 
-echo "[ALTER]  TABLE \"$BASETABLE\" RENAMED TO \"$TMP_BASETABLE\""
-# CREATE CLEAN BASE TABLE
-psql -U $DBUSER $DB -c "CREATE TABLE $BASETABLE ( LIKE $TMP_BASETABLE INCLUDING ALL ) ;" > /dev/null ||throw_ex "The table \"$BASETABLE\" cannot be created"
-echo "[CREATE] TABLE \"$BASETABLE\" ( LIKE \"$TMP_BASETABLE\" INCLUDING ALL )\n"
-
-
-## CREATE Partition tables and INSERT values to partitioned tables according to given INTERVAL parameter
-for DATE in $DATE_RANGE
-do 
+get_suffix() {
+    DATE=$1
     YEAR=`echo $DATE | cut -d"-" -f1`
     MONTH=`echo $DATE | cut -d"-" -f2`
     if [ "$TYPE" = "day" ]
@@ -73,20 +45,53 @@ do
         echo "Partition type should be day or month"
         exit
     fi
+}
 
-    #DATE_STR=`echo "date --date='$DATE $INTERVAL ' +%Y-%m-%d"`
+# Find the min and max date from the table
+DATE_RANGE=`psql -U $DBUSER -d $DB -At -c "SELECT to_char(generate_series('$START_DATE'::timestamp , '$END_DATE'::timestamp, '$INTERVAL'),'YYYY-MM-DD');"` ||throw_ex "Getting Date range array from DB"
+
+echo -e '\nTABLE: '$BASETABLE' \nDATE BETWEEN: '$START_DATE' AND '$END_DATE' \nINTERVAL: '$INTERVAL' \nDATE RANGE: '$DATE_RANGE'\n'
+
+
+TRIGGER_STATEMENT_START="
+CREATE OR REPLACE FUNCTION $TRIGGER()
+RETURNS TRIGGER AS \$\$
+BEGIN"
+
+TRIGGER_STATEMENT_END="
+    END IF;
+RETURN NULL;
+END;
+\$\$
+LANGUAGE plpgsql;
+"
+
+TRIGGER_STATEMENT_BODY=""
+
+# RENAME BASE TABLE AS TMP TABLE
+psql -U $DBUSER $DB -c "ALTER TABLE $BASETABLE RENAME TO $TMP_BASETABLE;" > /dev/null ||throw_ex "Cannot renamed the table $BASETABLE to $TMP_BASETABLE"
+echo "[ALTER]  TABLE \"$BASETABLE\" RENAMED TO \"$TMP_BASETABLE\""
+# CREATE CLEAN BASE TABLE
+psql -U $DBUSER $DB -c "CREATE TABLE $BASETABLE ( LIKE $TMP_BASETABLE INCLUDING ALL ) ;" > /dev/null ||throw_ex "The table \"$BASETABLE\" cannot be created"
+echo "[CREATE] TABLE \"$BASETABLE\" ( LIKE \"$TMP_BASETABLE\" INCLUDING ALL )"
+
+
+## CREATE Partition tables and INSERT values to partitioned tables according to given INTERVAL parameter
+for DATE in $DATE_RANGE
+do
+    get_suffix "$DATE"
     NEXT_DATE=`eval "date --date='$DATE $INTERVAL ' +%Y-%m-%d"`
-    
+
     ## CREATING partition tables
     CREATE_STATEMENT="CREATE TABLE $BASETABLE$suffix ( LIKE $BASETABLE INCLUDING ALL, CHECK ( $DIVISION_COLUMN >= DATE '$DATE' AND $DIVISION_COLUMN < DATE '$NEXT_DATE' ) ) INHERITS ($BASETABLE);"
     ## INSERTING partition values
-    INSERT_STATEMENT="INSERT INTO $BASETABLE$suffix ( SELECT * FROM $TMP_BASETABLE WHERE $DIVISION_COLUMN >= DATE '$DATE' AND $DIVISION_COLUMN < DATE '$NEXT_DATE' );" 
-    
+    INSERT_STATEMENT="INSERT INTO $BASETABLE$suffix ( SELECT * FROM $TMP_BASETABLE WHERE $DIVISION_COLUMN >= DATE '$DATE' AND $DIVISION_COLUMN < DATE '$NEXT_DATE' );"
+
     psql -U $DBUSER $DB -c "$CREATE_STATEMENT" > /dev/null 2>&1 ||throw_ex "The partitioned sub table \"$BASETABLE$suffix\" cannot be created"
     echo -n "[CREATE:SUB] TABLE $BASETABLE$suffix"
     psql -U $DBUSER $DB -c "$INSERT_STATEMENT" > /dev/null ||throw_ex "Inserting data into the table \"$BASETABLE$suffix\""
     echo " -> [INSERTED]"
-   
+
     if [ $TRIGGER_FLAG -eq 1 ]
     then
         TRIGGER_STATEMENT_BODY="$TRIGGER_STATEMENT_BODY
@@ -106,10 +111,17 @@ done
 
 TRIGGER_STATEMENT="$TRIGGER_STATEMENT_START""$TRIGGER_STATEMENT_BODY""$TRIGGER_STATEMENT_END"
 echo -n "$TRIGGER_STATEMENT" | psql -U $DBUSER $DB > /dev/null ||throw_ex "The trigger \"$TRIGGER\" cannot be created" ## Exception is not working 
-echo "\n[CREATE] TRIGGER \"$TRIGGER\""
+echo "[CREATE] TRIGGER \"$TRIGGER\""
 
 # CREATE TRIGGER 
 psql -U $DBUSER $DB -c "CREATE TRIGGER $BASETABLE""_trigger BEFORE INSERT on $BASETABLE for each row execute procedure $BASETABLE""_insert_trigger();" > /dev/null ||throw_ex "The trigger \"$BASETABLE""_insert_trigger\" cannot be created "
 echo "[CREATE] TRIGGER \"$BASETABLE""_trigger\""
 
-echo "\nThe table $BASETABLE partitioning process OK"
+echo "The table $BASETABLE partitioning process OK"
+
+
+
+
+
+##  var olan partition sistemi için süre uzatımı 
+##select proname,prosrc from pg_proc where proname='part_insert_trigger';
